12583112fdc1741492fa06bc265209630b2db36c
Jack Tsai <jtsai@nexify.com.hk>
2025-06-09 17:35:34 +0800

    Add prettier

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,15 @@
 // main.ts
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const vuetify = createVuetify({
   components,
   directives,
-})
+});
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).use(vuetify).mount('#app');
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
@@ -8,4 +8,4 @@ export default defineConfig({
       '@': '/src',
     },
   },
-})
+});
