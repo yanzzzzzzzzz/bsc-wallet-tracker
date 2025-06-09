11ac983b9ad65a41bb2e0ca0031816720cc31f75
Jack Tsai <jtsai@nexify.com.hk>
2025-06-09 17:32:15 +0800

    Add vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,17 @@
+// main.ts
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
