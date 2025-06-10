8edde2f9b12070248b3002ccd4542df096f11c6e
Jack Tsai <jtsai@nexify.com.hk>
2025-06-10 16:38:03 +0800

    add prettier

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,11 +1,7 @@
 <template>
   <v-container class="fill-height" max-width="900">
     <div>
-      <v-img
-        class="mb-4"
-        height="150"
-        src="@/assets/logo.png"
-      />
+      <v-img class="mb-4" height="150" src="@/assets/logo.png" />
 
       <div class="mb-8 text-center">
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
@@ -27,14 +23,13 @@
             </template>
 
             <template #title>
-              <h2 class="text-h5 font-weight-bold">
-                Get started
-              </h2>
+              <h2 class="text-h5 font-weight-bold">Get started</h2>
             </template>
 
             <template #subtitle>
               <div class="text-subtitle-1">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>components/HelloWorld.vue</v-kbd>.
+                Change this page by updating
+                <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>components/HelloWorld.vue</v-kbd>.
               </div>
             </template>
           </v-card>
