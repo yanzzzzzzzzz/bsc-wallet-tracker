c7471d0395098b39c9b32c6b628ca809e1a9ce6f
Jack Tsai <jtsai@nexify.com.hk>
2025-06-17 10:03:50 +0800

    Add get block no by time using etherscan api
    Add prettier

---
+++ backend/src/api/etherscan.api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+const API_KEY = '';
+const api = axios.create({
+  baseURL: 'https://api.etherscan.io/api',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+interface responseModel {
+  status: string;
+  message: string;
+  result: string;
+}
+export const getBlockNoByTime = async (): Promise<string> => {
+  const now = new Date();
+  const timestamp = Math.floor(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()) / 1000,
+  );
+  const response = await api.get('', {
+    params: {
+      module: 'block',
+      action: 'getblocknobytime',
+      timestamp: timestamp,
+      closest: 'before',
+      apikey: API_KEY,
+    },
+  });
+  const data = response.data;
+  if (data.status === '0') {
+    throw new Error(`API Error: ${data.message}`);
+  }
+  return data.result;
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { getBlockNoByTime } from './api/etherscan.api';
 const app = express();
 const port = 5678;
 
@@ -7,6 +7,14 @@ app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from TypeScript API!' });
+});
+
+app.get('/api/transactions/:walletAddress', async (req, res) => {
+  const walletAddress = req.params.walletAddress;
+  const blockNo = await getBlockNoByTime();
+
+  console.log(blockNo);
+  res.send(blockNo);
 });
 
 app.listen(port, () => {
